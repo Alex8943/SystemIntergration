218d192fa9d0653ad4d4b252f5f0610be8db7976
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2023-04-18 18:36:25 +0200

    add a gitignore

--- 18_mandatory2/part1/client/auth.js
+++ 18_mandatory2/part1/client/auth.js
@@ -1,9 +1,9 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'; 
 
+
 const GOOGLE_CLIENT_ID = "**********************************"; 
 const GOOGLE_CLIENT_SECRET = "**********************************";
-
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
