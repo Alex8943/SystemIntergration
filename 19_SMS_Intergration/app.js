3ef45ea015a8a81e69984ae45accc6923a88b2b8
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2023-07-22 15:04:20 +0200

    SMS intergration update

--- 19_SMS_Intergration/app.js
+++ 19_SMS_Intergration/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+
 const app = express(); 
 
+require('dotenv').config();
 
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Set environment variables for your credentials
-// Read more at http://twil.io/secure
-const accountSid = "AC6936b1eb7662f0b39efa1b1a2f3e7f55";
-const authToken = "2d8bc79d275f54fc47643d0ecee07380"
-const client = require("twilio")(accountSid, authToken);
+const accountSid = process.env.accountSid; 
+const authToken = process.env.authToken;
+const client = require('twilio')(accountSid, authToken);
+const fromPhoneNumber = process.env.TWILIO_FROM_PHONE_NUMBER;
+const toPhoneNumber = process.env.TWILIO_TO_PHONE_NUMBER; 
+
 client.messages
-  .create({ body: "Hello from Twilio", from: "+16813293524", to: "+4550694489" })
+  .create({ body: "Hello from Twilio", from: fromPhoneNumber, to: toPhoneNumber })
     .then(message => console.log(message.body));
 
 app.listen(3000, () => console.log("Server is running on port " + 3000));
